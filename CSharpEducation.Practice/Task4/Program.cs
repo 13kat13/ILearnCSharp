ce87301973f349494515dc9e9637a918a1f59d89
13kat13 <155016982+13kat13@users.noreply.github.com>
2024-11-14 16:46:46 +0400

    задание 4. Напишите программу, которая считывает из консоли фразу и сохраняет её. Затем считывает вторую фразу и выводит на экран консоли обе этих фразы, объединённые в одно предложение.

---
+++ CSharpEducation.Practice/Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ваше имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Ваше отчество:");
+            string namepatronymic = Console.ReadLine();
+            Console.WriteLine(name + namepatronymic + ", приветствую Вас!");
+            Console.ReadKey();
+        }
+    }
+}
