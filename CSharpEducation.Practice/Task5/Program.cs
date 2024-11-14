30d21be2368d80ad50b1b5a03c968c47560b36f5
13kat13 <155016982+13kat13@users.noreply.github.com>
2024-11-14 16:59:29 +0400

    задание 5. вычисление формулы площади круга

---
+++ CSharpEducation.Practice/Task5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi= 3.1415926535;
+            double radius = 3;
+            double s=pi*radius*radius;
+            Console.WriteLine("Площадь круга с радиусом "+radius+" равна "+s);
+            Console.ReadKey();
+        }
+    }
+}
