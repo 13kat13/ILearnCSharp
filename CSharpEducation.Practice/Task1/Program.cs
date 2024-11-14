316f470ece1ed017df276ea28d6af5c1059a12bd
13kat13 <155016982+13kat13@users.noreply.github.com>
2024-11-14 16:21:22 +0400

    задание 1. Объявите переменную следующего вида

---
+++ CSharpEducation.Practice/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //целочисленная переменная, обозначающая возраст
+            byte age;
+            //строковая переменная для обозначения имени
+            string name;
+            //строковая переменная для обозначения названия компании
+            object company_names;
+            //переменная логического типа
+            bool alive;
+            //переменная типа с плавающей точкой для обозначения веса тела человека
+            float weight;
+        }
+    }
+}
