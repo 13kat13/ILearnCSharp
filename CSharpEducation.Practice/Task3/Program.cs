3c6e6ba45a0b17927518af783a74c5364730f2a6
13kat13 <155016982+13kat13@users.noreply.github.com>
2024-11-14 16:40:31 +0400

    задание 3. Напишите программу, которая считывает из консоли ваше имя и выводит приветственное сообщение

---
+++ CSharpEducation.Practice/Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как Вас зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine(name + ", привет!");
+            Console.ReadKey();
+        }
+    }
+}
