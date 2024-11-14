36fad3505da4a8249ee59049503b8b83c6a708a2
13kat13 <155016982+13kat13@users.noreply.github.com>
2024-11-14 16:29:41 +0400

    задание 2. Объявите и инициализируйте переменную следующего вида

---
+++ CSharpEducation.Practice/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //переменная для обозначения возраста человека, имеющая значение вашего возраста.
+            byte age=22;
+            //переменная, обозначающая ваше имя.
+            string name="Катя";
+            //переменная для обозначения температуры за окном
+            float temperature = -2.6f;
+            //переменная логического типа, обозначающая принадлежность к женскому полу.
+            bool girl = true;
+        }
+    }
+}
