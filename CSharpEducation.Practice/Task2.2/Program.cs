1a952cf821f4fc5fd317f49d68721145d8358517
13kat13 <155016982+13kat13@users.noreply.github.com>
2024-11-20 23:06:44 +0400

    задание 2.2. конвертация

---
+++ CSharpEducation.Practice/Task2.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //a.метры в километры
+            Console.WriteLine("введите число в метрах");
+            double m = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(m/1000+" километров");
+            //b.километры в сантиметры
+            Console.WriteLine("введите число в километрах");
+            double km = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(km * 100000 + " сантиметров");
+            //c.м / с в км/ ч
+            Console.WriteLine("введите число в метрах в секунду");
+            double mvs = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(mvs * 10/36 + " километров в час");
+            //d.градусы C в градусы F
+            Console.WriteLine("введите градусы в цельсиях");
+            double с = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(с * 1.8+32 + " фаренгейта");
+            Console.ReadKey();
+        }
+    }
+}
