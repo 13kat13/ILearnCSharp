0420ddb79b6c1fde78c682d34779fb540fca9a3a
13kat13 <155016982+13kat13@users.noreply.github.com>
2024-11-27 22:53:38 +0400

    задание 2.10. методы для создания и вывода массива

---
+++ CSharpEducation.Practice/Task 2.10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2._10
+{
+    internal class Program
+    {
+        //Напишите метод, который создаёт массив целых чисел и возвращает его. Размер массива нужно
+        //передавать в качестве аргумента. Вдобавок напишите метод, который выводит переданный массив на экран консоли
+        static int[] CreateArray(int a)
+        {
+            int[] array = new int[a];
+            for(int i = 0; i < array.Length; i++) 
+            {
+                array[i] = i+1;
+            }
+            return array;
+        }
+        static void Vivod(int[] array)
+        {
+            for(int i = 0;i<array.Length;i++)
+                Console.Write(array[i]+" ");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите размер массива");
+            int raz=Convert.ToInt32(Console.ReadLine());
+            int[] matr=CreateArray(raz);
+            Vivod(matr);
+            Console.ReadKey();
+        }
+    }
+}
