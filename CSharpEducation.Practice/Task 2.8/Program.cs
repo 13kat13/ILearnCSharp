06309dd42e4b8874bdd5f71492d1a99f10d58426
13kat13 <155016982+13kat13@users.noreply.github.com>
2024-11-27 22:18:21 +0400

    задание 2.8. изменения регистров

---
+++ CSharpEducation.Practice/Task 2.8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Task_2._8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Напишите программу, которая переводит строку в разные регистры
+            //a.в верхний регистр: f->F
+            Console.WriteLine("введите строку которую хотите перевести в верхний регистр");
+            string s1 = Console.ReadLine();
+            Console.WriteLine(s1.ToUpper());
+            //b.в нижний регистр: F->f
+            Console.WriteLine("введите строку которую хотите перевести в нижний регистр");
+            string s2 = Console.ReadLine();
+            Console.WriteLine(s2.ToLower());
+            //c.делает заглавную букву в слове: привет->Привет
+            Console.WriteLine("введите строку которую хотите вывести с заклавной буквы");
+            string s3 = Console.ReadLine();
+            string sn =Convert.ToString(s3[0]);
+            string sc= s3.Substring(1);
+            Console.WriteLine(sn.ToUpper()+sc);
+            Console.ReadKey();
+        }
+    }
+}
